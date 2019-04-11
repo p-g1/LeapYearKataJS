443c8a903bc19ee40f31539ecf53dd40935c5fbd
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-11 08:39:29 +0100

    removing comments

--- LeapYear/LeapYearKata.js
+++ LeapYear/LeapYearKata.js
@@ -1,9 +1,8 @@
 exports.isLeapYear = year => {
-  
   Number.prototype.isDivisibleBy = divisor => {
     return year % divisor == 0;
-  }
-  
+  };
+
   if (year.isDivisibleBy(100) && !year.isDivisibleBy(400)) {
     return false;
   }
@@ -14,9 +13,3 @@ exports.isLeapYear = year => {
 
   return false;
 };
-
-//renamed files
-
-//changed file structure. Included tests within special file within leap year as that is a common practice in JS
-
-//extended Number prototype with new method to allow semantic read within function.
